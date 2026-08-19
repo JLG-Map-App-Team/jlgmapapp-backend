203578cfbe81712780f687bc03bc9fa535d071ba
wassupitzmaha <mahau1833@gmail.com>
2026-08-19 11:47:22 -0400

    updated the yaml with 4xx error response and made the endpoint public

---
+++ src/types/segments.ts
@@ -0,0 +1,8 @@
+import type { components } from './api';
+
+export type SegmentFeatureCollection = components['schemas']['SegmentFeatureCollection'];
+export type SegmentFeature           = components['schemas']['SegmentFeature'];
+export type SegmentProperties        = components['schemas']['SegmentProperties'];
+export type LineStringGeometry       = components['schemas']['LineStringGeometry'];
+export type Position                 = components['schemas']['Position'];
+export type Problem                  = components['schemas']['Problem'];
